31b55fe3625e3c6d40ec7549515d5473612f50db
Paweł Janecki <33122103+PawelJa@users.noreply.github.com>
2019-09-25 09:58:58 +0200

    DI - constructor injection - Java annotations

---
+++ src/com/spring/entities/impl/TennisCoach.java
@@ -0,0 +1,27 @@
+package com.spring.entities.impl;
+
+import com.spring.entities.Coach;
+import com.spring.services.FortuneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+    private FortuneService fortuneService;
+
+    @Autowired
+    public TennisCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
+}
