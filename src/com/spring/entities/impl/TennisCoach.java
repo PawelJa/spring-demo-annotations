d7adae2748de8173aed58f67770c0549862f1b7e
Paweł Janecki <33122103+PawelJa@users.noreply.github.com>
2019-09-25 11:12:33 +0200

    DI - field injection - Java annotation

--- src/com/spring/entities/impl/TennisCoach.java
+++ src/com/spring/entities/impl/TennisCoach.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+    @Autowired
     private FortuneService fortuneService;
 
 //    @Autowired
@@ -20,11 +21,11 @@ public class TennisCoach implements Coach {
     }
 
     // define a setter method
-    @Autowired
-    public void doSomeCrazyStuff(FortuneService fortuneService) {
-        System.out.println("TennisCoach: inside doSomeCrazyStuff() method");
-        this.fortuneService = fortuneService;
-    }
+//    @Autowired
+//    public void doSomeCrazyStuff(FortuneService fortuneService) {
+//        System.out.println("TennisCoach: inside doSomeCrazyStuff() method");
+//        this.fortuneService = fortuneService;
+//    }
 
     @Override
     public String getDailyWorkout() {
