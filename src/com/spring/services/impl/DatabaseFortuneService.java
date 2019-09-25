bdb97d1226424ed8cfed7799d32633481c4e7c88
Paweł Janecki <33122103+PawelJa@users.noreply.github.com>
2019-09-25 11:32:51 +0200

    DI - qualifiers - Java annotation

---
+++ src/com/spring/services/impl/DatabaseFortuneService.java
@@ -0,0 +1,13 @@
+package com.spring.services.impl;
+
+import com.spring.services.FortuneService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseFortuneService implements FortuneService {
+
+    @Override
+    public String getFortune() {
+        return null;
+    }
+}
